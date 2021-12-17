5b1a322de7e1d39d067479472d96082c68ed0455
Asain Satanov <satanov.asain@mail.ru>
2021-12-17 22:00:07 +0500

    Spinner & Error case added

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,9 @@
+import img from './ErrorMessage.gif';
+
+const ErrorMessage=()=>{
+    return(
+        <img src={img} alt='Error' style={{objectFit:'contain',display:'block', width:'250px',height:'250px', margin:'0 auto'}}/>
+    )
+}
+
+export default ErrorMessage;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage.js';
 import MarvelService from '../../services/MarvelService';
 import './randomChar.scss';
 
@@ -12,7 +13,8 @@ class RandomChar extends Component {
     }
     state={
        char:{},
-       loading : true
+       loading : true,
+       error: false
     }
     marvelService= new MarvelService(); 
 
@@ -24,14 +26,28 @@ class RandomChar extends Component {
         const id=Math.floor(Math.random()*(1011400-1011000)+1011000);
         this.marvelService
             .getCharacter(id)
-                .then(this.onCharLoaded)
+            .then(this.onCharLoaded)
+            .catch(this.onError)
+    }
+
+    onError=()=>{
+        this.setState({
+            loading:false,
+            error:true
+        })
     }
 
     render(){
-        const {char, loading} = this.state;
+        const {char, loading,error} = this.state;
+
+        const errorMessage= error?<ErrorMessage/>:null;
+        const spinner = loading?<Spinner/>:null;
+        const content = !(error||loading)?<View char={char}/>:null;
         return (
             <div className="randomchar">
-               {loading?<Spinner/>:<View char={char}/>}
+               {errorMessage}
+               {spinner}
+               {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
